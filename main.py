0ea39e5d76a016d7e37f81e13b30c8b357724f51
xxxxxxxxxx <xxxxxxxxxx@cn330>
2022-09-16 10:38:28 +0700

    Add endpoint /world

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+import csv
+
 # For tutorial on FastAPI
 # See https://fastapi.tiangolo.com/tutorial/first-steps/
 from fastapi import FastAPI
+
+# get country data from csv file  
+filename = "world_table_country.csv"
+with open(filename, "r") as csv_file:
+    csv_reader = csv.reader(csv_file)
+    headers = next(csv_reader)
+    data_country = [{k: v for (k, v) in zip(headers, row)} for row in csv_reader]
 
 # create an instance of class FastAPI named "app"
 app = FastAPI()
@@ -9,3 +18,10 @@ app = FastAPI()
 @app.get("/")
 async def read_root() -> dict:
     return {"message": "Hello there"}
+
+
+# define function that handles "GET" request with endpoint "/world"
+# list all countries
+@app.get("/world")
+async def read_countries() -> dict:
+    return {"result": data_country}
