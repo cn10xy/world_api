6d2aa46267ea7cf2fe8ef8e8667a99ccd327e0a3
Nawin Somyat <snawin@engr.tu.ac.th>
2023-09-21 15:38:50 +0700

    Add endpoint /world/city/{name}

--- main.py
+++ main.py
@@ -11,6 +11,13 @@ with open(filename, "r") as csv_file:
     headers = next(csv_reader)
     data_country = [{k: v for (k, v) in zip(headers, row)} for row in csv_reader]
 
+# get city data from csv file
+filename = "world_table_city.csv"
+with open(filename, "r") as csv_file:
+    csv_reader = csv.reader(csv_file)
+    headers = next(csv_reader)
+    data_city = [{k: v for (k, v) in zip(headers, row)} for row in csv_reader]
+
 # create an instance of class FastAPI named "app"
 app = FastAPI()
 
@@ -19,8 +26,19 @@ app = FastAPI()
 async def read_root() -> dict:
     return {"message": "Hello there"}
 
+
 # define function that handles "GET" request with endpoint "/world"
 # list all countries
 @app.get("/world")
 async def read_countries() -> dict:
     return {"result": data_country}
+
+
+# define function that handles "GET" request with endpoint "/world/city/{name}"
+# "/world/city/{name}" is a "path paramter" endpoint
+@app.get("/world/city/{name}")
+async def read_city(name: str) -> dict:
+    for row in data_city:
+        if row["Name"].lower() == name.lower():
+            return {"result": row}
+    return {"result": {}}
